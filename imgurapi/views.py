f5b687609a756b4d2e9d9f42151232254a394752
Stivi7 <stivi.ndoni@gmail.com>
2017-11-22 22:31:22 +0100

    pagination errors

--- imgurapi/views.py
+++ imgurapi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from imgurpython import ImgurClient
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 client_id = '7107129cd0a4a00'
 client_secret = '8400a4ead045a798b95bb3e0d86f913d8cf3e5bc'
@@ -9,6 +10,28 @@ refresh_token = 'df27297843576325e8b40e36725463006c3ea08f'
 client = ImgurClient(client_id, client_secret, access_token, refresh_token)
 
 def search(request, data):
-    items = client.gallery_search(data, advanced=any, sort='time', window='all', page=0)
+    items = client.gallery_search(data, advanced=None, sort='time', page=0)
+    links = []
+
     for item in items:
-        return HttpResponse(item.link)
+        json = {
+            "link": item.link,
+            "title": item.title,
+            "img-id": item.id,
+            "views": item.views,
+        }
+        links.append(json)
+
+        # value = request.GET.get('offset', '')
+        # paginator = Paginator(links, value)
+        # try:
+        #     page = paginator.page[value]
+        # except PageNotAnInteger:
+        #     page = paginator.page[1]
+        # except EmptyPage:
+        #     page = paginator.page(paginator.num_page)
+     
+
+
+    return JsonResponse(links, safe=False)
+    
